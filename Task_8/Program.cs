9e5807f91296946b42dc0f4e6e7c35e082234c81
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-20 19:58:36 +0900

    Update Program.cs

--- Task_8/Program.cs
+++ Task_8/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,14 +11,14 @@ namespace Task_8
         static void Main(string[] args)
         {
             int timeToOnePeople = 10;
-            int oneOurValue = 60;
+            int minutesInOneHour = 60;
             Console.WriteLine("Вы пришли в поликлиннику и пытаетесь посчитать сколько времени вы потратите. Сколько человек вы видите в данный момент перед собой?");
             int oldPeopleCounter = Convert.ToInt32(Console.ReadLine());
             int timeCounter = oldPeopleCounter * timeToOnePeople;
-            int oursAmount = timeCounter / oneOurValue;
-            int minunesAmount = timeCounter % oneOurValue;
+            int hoursOfWaiting = timeCounter / minutesInOneHour;
+            int minunesOfWaiting = timeCounter % minutesInOneHour;
             Console.WriteLine("Судя по всему вам придётся проторчать тут не менее " + timeCounter +" минут ");
-            Console.WriteLine("Иначе - " + oursAmount + "часа/ов и " + minunesAmount + " минут");
+            Console.WriteLine("Иначе - " + hoursOfWaiting + "часа/ов и " + minunesOfWaiting + " минут");
         }
     }
 }
