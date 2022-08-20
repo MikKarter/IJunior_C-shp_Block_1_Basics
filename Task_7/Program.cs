81475bb6513abd10ea5d152ae990afdd58103b65
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-20 19:11:28 +0900

    Update Program.cs

--- Task_7/Program.cs
+++ Task_7/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,14 +11,40 @@ namespace Task_7
         static void Main(string[] args)
         {
             int priceCrystall = 25;
-            int counterCrystallInInventory;
-            int counterGoldInInventory;
+            int crystallInInventory;
+            int goldInInventory;
+            int howMuchCrystallWillBuy;
             Console.WriteLine("Welcome, the Traveler. In my shop you can buy the crystalls! How much gold you have?");
-            counterGoldInInventory = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Fine! 1 crystall will cost you " + priceCrystall);
-            counterCrystallInInventory = counterGoldInInventory / priceCrystall;
-            counterGoldInInventory = counterGoldInInventory % priceCrystall;
-            Console.WriteLine("SYSTEM MESSAGE \nYou bought " + Convert.ToString(counterCrystallInInventory) + " crystalls" + "\nAfter purchases you have " + Convert.ToString(counterGoldInInventory) + " gold");
+            goldInInventory = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fine! 1 crystall will cost you " + priceCrystall + ". How much crystalls you wil buying?");
+            howMuchCrystallWillBuy = Convert.ToInt32(Console.ReadLine());
+            if (goldInInventory < (howMuchCrystallWillBuy*priceCrystall))
+                {
+                Console.WriteLine("Sorry, you don't have so much gold");
+                int offerCrystals = goldInInventory / priceCrystall;
+                Console.WriteLine(" I can offer only " + offerCrystals + " for you, will buy? \n Print Yes/No");
+                string willBuyCrysstalsToOffer = Console.ReadLine();
+                if (willBuyCrysstalsToOffer == "Yes")
+                    {
+                    crystallInInventory = offerCrystals;
+                    goldInInventory = goldInInventory % (crystallInInventory*priceCrystall);
+                    Console.WriteLine("SYSTEM MESSAGE \nYou bought " + Convert.ToString(crystallInInventory) + " crystalls" + "\nAfter purchases you have " + Convert.ToString(goldInInventory) + " gold");
+                    }
+                if (willBuyCrysstalsToOffer == "No")
+                    {
+                    Console.WriteLine("Sorry, maybe next time...");
+                    }
+                if (willBuyCrysstalsToOffer != "Yes" & willBuyCrysstalsToOffer != "No")
+                    {
+                    Console.WriteLine("Sorry, i dont undeststand this message"); 
+                    }
+                }
+            else
+                {
+                crystallInInventory = howMuchCrystallWillBuy;
+                goldInInventory = goldInInventory % (crystallInInventory * priceCrystall);
+                Console.WriteLine("SYSTEM MESSAGE \nYou bought " + Convert.ToString(crystallInInventory) + " crystalls" + "\nAfter purchases you have " + Convert.ToString(goldInInventory) + " gold");
+            };
         }
     }
 }
