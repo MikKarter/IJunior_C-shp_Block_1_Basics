5c8b293ebb5bdba0731e3327df40998c4ad17942
MikKarter <66348029+MikKarter@users.noreply.github.com>
2022-08-20 20:07:02 +0900

    Update Program.cs

--- Task_7/Program.cs
+++ Task_7/Program.cs
@@ -18,33 +18,9 @@ namespace Task_7
             goldInInventory = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Fine! 1 crystall will cost you " + priceCrystall + ". How much crystalls you wil buying?");
             howMuchCrystallWillBuy = Convert.ToInt32(Console.ReadLine());
-            if (goldInInventory < (howMuchCrystallWillBuy*priceCrystall))
-                {
-                Console.WriteLine("Sorry, you don't have so much gold");
-                int offerCrystals = goldInInventory / priceCrystall;
-                Console.WriteLine(" I can offer only " + offerCrystals + " for you, will buy? \n Print Yes/No");
-                string willBuyCrysstalsToOffer = Console.ReadLine();
-                if (willBuyCrysstalsToOffer == "Yes")
-                    {
-                    crystallInInventory = offerCrystals;
-                    goldInInventory = goldInInventory % (crystallInInventory*priceCrystall);
-                    Console.WriteLine("SYSTEM MESSAGE \nYou bought " + Convert.ToString(crystallInInventory) + " crystalls" + "\nAfter purchases you have " + Convert.ToString(goldInInventory) + " gold");
-                    }
-                if (willBuyCrysstalsToOffer == "No")
-                    {
-                    Console.WriteLine("Sorry, maybe next time...");
-                    }
-                if (willBuyCrysstalsToOffer != "Yes" & willBuyCrysstalsToOffer != "No")
-                    {
-                    Console.WriteLine("Sorry, i dont undeststand this message"); 
-                    }
-                }
-            else
-                {
-                crystallInInventory = howMuchCrystallWillBuy;
-                goldInInventory = goldInInventory % (crystallInInventory * priceCrystall);
-                Console.WriteLine("SYSTEM MESSAGE \nYou bought " + Convert.ToString(crystallInInventory) + " crystalls" + "\nAfter purchases you have " + Convert.ToString(goldInInventory) + " gold");
-            };
+            crystallInInventory = howMuchCrystallWillBuy;
+            goldInInventory = goldInInventory - crystallInInventory * priceCrystall;
+            Console.WriteLine("You have " + crystallInInventory + " crystall and " + goldInInventory + " gold");
         }
     }
 }
